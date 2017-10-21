75cc292b593cebc027c89f01ea94891444fa9def
ykadir42 <ykadir42@gmail.com>
2017-10-20 23:59:45 -0400

    added some test cases for SongNode

--- main.c
+++ main.c
@@ -17,19 +17,21 @@
 
 #define pn() p("")
 
+#define pt() printf("\t\t")
+
 void debug_SongNode() {
     Song song1 = SongClass.new("Name", "Artists");
     Song song2 = SongClass.new("Hello", "World");
     //Song song3 = SongClass.new("Song", "Bob");
-    
+
     p("inserting in order");
-    
+
     SongNode *node = SongNodeClass.new_sentinel(song1);
     node = node->c->insert_in_order(node, song2);
     printf("%p\n", node);
     //node = node->c->insert_in_order(node, song3);
     node->c->print(node);
-    
+
     node->c->free(node);
 }
 
@@ -37,31 +39,31 @@ void debug_SongLibrary() {
     p("creating library");
     SongLibrary *const library = SongLibraryClass.new();
     p("created library");
-    
+
     p("creating song1");
     Song song1 = SongClass.new("Title", "Artist");
     p("created song1");
-    
+
     p("printing song1");
     song1.c->print(song1);
     p("printed song1");
-    
+
     p("adding song1");
     library->c->add_song(library, song1);
     p("added song1");
 
 //    library->c->add_song(library, SongClass.new("Hello", "World"));
 //    library->c->add_song(library, SongClass.new("Khyber", "Sen"));
-    
+
     library->c->add_songs_from_csv(library, "songs.csv");
-    
+
     p("printing library");
     library->c->print(library);
     p("printed library");
-    
+
     const SongNode *node = library->c->songs_by_artist_letter(library, 'A');
     node->c->print(node);
-    
+
     library->c->free(library);
 }
 
@@ -69,22 +71,106 @@ void test_SongNode() {
     p("====================");
     p("TESTING SONG LINKED LIST");
     p("====================\n");
-    
+
     PRE_SONG_STRING = "\t\t";
-    
+
     // tests
     const Song song1 = SongClass.new("SongName", "Artist");
-    
+
+	p("Testing SongNode.new_sentinel()");
+	p("\tshould print just song1");
     SongNode *songs = SongNodeClass.new_sentinel(song1);
-    
-    songs->c->free(songs);
-    
+    songs->c->print(songs);
+	pn();
+
+	p("Testing SongNode constructor");
+    SongNode *test = SongNodeClass.new(song1, songs);
+	test->c->print(test);
+	pn();
+
+	p("Testing SongNode.free_nodes_only()");
+	p("\tshould print a null pointer");
+	pt();
+    printf("%p\n",test->c->free_nodes_only(test));
+	SongClass.free(song1);
+	pn();
+
     SongLibrary *const library = SongLibraryClass.new();
     library->c->add_songs_from_default_csv(library);
     const SongNode *const all_songs = library->c->all_songs(library);
-    
-    all_songs->c->free_nodes_only((SongNode *) all_songs);
-    
+	const Song songNew = SongClass.new("NewSong", "NewArtist");
+
+	p("Testing SongNode.length()");
+	p("\tshould print 27, the number of songs in our playlist");
+	pt();
+    printf("length: %zu\n", all_songs->c->length(all_songs));
+	pn();
+
+	p("Testing SongNode.insert_front()");
+	p("\tshould have new song at the front of the library");
+	SongNode *new = (SongNode *)all_songs->c->insert_front(all_songs, songNew);
+    new->c->print(new);
+	pn();
+
+	p("Testing SongNode.remove_front()");
+	p("\tshould now remove previously added song from the library");
+    new = (SongNode *)new->c->remove_front(new);
+	new->c->print(new);
+	pn();
+
+	p("Testing SongNode.insert_in_order()");
+	p("\tshould put the new song in the right place");
+	new = (SongNode *)all_songs->c->insert_in_order(all_songs, songNew);
+    new->c->print(new);
+	pn();
+	/*
+	p("Testing SongNode.find_by_name()");
+	p("\tshould print 27, the number of songs in our playlist");
+	pt();
+    printf("length: %zu\n", all_songs->c->length(all_songs));
+	pn();
+
+	p("Testing SongNode.find_by_artist()");
+	p("\tshould print 27, the number of songs in our playlist");
+	pt();
+    printf("length: %zu\n", all_songs->c->length(all_songs));
+	pn();
+
+	p("Testing SongNode.get()");
+	p("\tshould print 27, the number of songs in our playlist");
+	pt();
+    printf("length: %zu\n", all_songs->c->length(all_songs));
+	pn();
+
+	p("Testing SongNode.get_random_with_length()");
+	p("\tshould print 27, the number of songs in our playlist");
+	pt();
+    printf("length: %zu\n", all_songs->c->length(all_songs));
+	pn();
+
+	p("Testing SongNode.get_random()");
+	p("\tshould print 27, the number of songs in our playlist");
+	pt();
+    printf("length: %zu\n", all_songs->c->length(all_songs));
+	pn();
+
+	p("Testing SongNode.remove_song()");
+	p("\tshould print 27, the number of songs in our playlist");
+	pt();
+    printf("length: %zu\n", all_songs->c->length(all_songs));
+	pn();
+
+	p("Testing SongNode.free_nodes_maybe()");
+	p("\tshould print 27, the number of songs in our playlist");
+	pt();
+    printf("length: %zu\n", all_songs->c->length(all_songs));
+	pn();
+	*/
+	p("Testing SongNode.free()");
+	p("\tshould print a null pointer");
+	pt();
+    printf("%p\n", all_songs->c->free((SongNode *) all_songs));
+
     p("====================");
     p("SONG LINKED LIST PASSED ALL TESTS");
     p("====================\n");
@@ -94,33 +180,33 @@ void test_SongLibrary() {
     p("====================");
     p("TESTING SONG LIBRARY");
     p("====================\n");
-    
+
     PRE_SONG_STRING = "\t\t";
-    
+
     p("Testing SongLibrary constructor");
     SongLibrary *const library = SongLibraryClass.new();
     pn();
-    
+
     p("Testing SongLibrary.add_songs_from_csv(),");
     p("\twhich tests SongLibrary.add_song()");
     library->c->add_songs_from_default_csv(library);
     pn();
-    
+
     p("Testing SongLibrary.print(),");
     p("\tshould print all songs in alphabetic order by artist, then song name");
     library->c->print(library);
     pn();
-    
+
     p("Testing SongLibrary.print_by_letter('A'),");
     p("\tshould print all songs by 'A' in alphabetic order");
     library->c->print_by_letter(library, 'A');
     pn();
-    
+
     p("Testing SongLibrary.print_by_artist(\"Alz\"),");
     p("\tshould print all songs by \"Alz\" in alphabetic order");
     library->c->print_by_artist(library, "Alz");
     pn();
-    
+
     p("Testing SongLibrary.find_song(),");
     p("\twhich uses SongLibrary.find_by_artist() and SongLibrary.find_by_name()");
     const Song song = SongClass.new("Cheap Thrills", "Sia");
@@ -131,18 +217,18 @@ void test_SongLibrary() {
     printf("\tFound song %s\n", song_str2);
     free(song_str2);
     pn();
-    
+
     printf("Testing duplicate songs by re-adding %s\n", DEFAULT_SONGS_CSV);
     library->c->add_songs_from_default_csv(library);
     library->c->print(library);
     pn();
-    
+
     p("Testing SongLibrary.shuffle_and_print() for 10 random songs");
     library->c->shuffle_and_print(library, 10);
     p("\tHopefully these are all random.");
     p("\tTesting for the correct random distribution would be much harder.");
     pn();
-    
+
     p("Testing SongLibrary.remove_song()");
     p("\tprinting library before removal...");
     library->c->print(library);
@@ -153,10 +239,10 @@ void test_SongLibrary() {
     printf("\tso now printing all songs by letter '%c' from %s...\n", *song.artist, song_str1);
     library->c->print_by_letter(library, *song.artist);
     pn();
-    
+
     song.c->free(song);
     free(song_str1);
-    
+
     p("Testing SongLibrary.remove_all()");
     p("\tprinting library before removal...");
     library->c->print(library);
@@ -164,13 +250,13 @@ void test_SongLibrary() {
     p("\tprinting library again to make sure it's empty");
     library->c->print(library);
     pn();
-    
+
     p("Testing SongLibrary.free()");
     library->c->add_songs_from_default_csv(library);
     library->c->free(library);
     p("\tvalgrind also found no memory leaks");
     pn();
-    
+
     p("====================");
     p("SONG LIBRARY PASSED ALL TESTS");
     p("====================\n");
@@ -178,15 +264,15 @@ void test_SongLibrary() {
 
 int main() {
     srand((unsigned int) time(NULL));
-    
+
     #if (DEBUG)
     debug_song_node();
     debug_library();
     #endif
-    
+
     test_SongNode();
-    test_SongLibrary();
-    
+    //test_SongLibrary();
+
     return EXIT_SUCCESS;
 }
 
